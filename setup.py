e0a1d5021ea44b9edc501568920415f1c580690b
ulif <uli@gnufix.de>
2018-12-07 20:08:27 +0100

    Add minimal setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sb_finder',
+      version='0.1dev0',
+      description='StalkerBuster finder',
+      author='ulif and datenzwerg.in',
+      author_email='stalkerbuster@gnufix.de',
+      url='https://github.com/stalkerbuster/sb_finder',
+      package_dir = {'': 'src'},
+      packages=['sb_finder'],
+     )
