5cf844b11cb4ed5227d4104d09adcc3b299ee0c4
ulif <uli@gnufix.de>
2019-01-07 03:14:28 +0100

    Add test for matchin certain IPs.

--- tests/test_sb_finder.py
+++ tests/test_sb_finder.py
@@ -12,3 +12,12 @@ def test_matches_filter_detects_ip_packets():
     bpf_tcp = pcapy.compile(src.datalink(), 1350, "tcp", 0, 1)
     assert matches_filter(data, bpf_ip) is True
     assert matches_filter(data, bpf_tcp) is False
+
+
+def test_matches_filter_detects_dst():
+    # we can filter by destination ip
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    src = pcapy.open_offline(os.path.join(path, 'single_ip.pcap'))
+    header, data = src.next()
+    bpf = pcapy.compile(src.datalink(), 1350, "dst 4.3.2.1", 0, 1)
+    assert matches_filter(data, bpf) is True
