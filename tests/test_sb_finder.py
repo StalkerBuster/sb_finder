5e7eaa1d243a455602997f98d5f0f6639692507c
ulif <uli@gnufix.de>
2019-01-14 19:36:32 +0100

    Flatten tests.

--- tests/test_sb_finder.py
+++ tests/test_sb_finder.py
@@ -7,8 +7,7 @@ SAMPLES_PATH=os.path.abspath(os.path.dirname(__file__))
 
 def test_matches_filter_detects_ip_packets():
     # we can detect certain simple packets.
-    path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-    src = pcapy.open_offline(os.path.join(path, 'single_ip.pcap'))
+    src = pcapy.open_offline(os.path.join(SAMPLES_PATH, 'single_ip.pcap'))
     header, data = src.next()
     bpf_ip = pcapy.compile(src.datalink(), 1350, "ip", 0, 1)
     bpf_tcp = pcapy.compile(src.datalink(), 1350, "tcp", 0, 1)
@@ -18,8 +17,7 @@ def test_matches_filter_detects_ip_packets():
 
 def test_matches_filter_detects_dst():
     # we can filter by destination ip
-    path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-    src = pcapy.open_offline(os.path.join(path, 'single_ip.pcap'))
+    src = pcapy.open_offline(os.path.join(SAMPLES_PATH, 'single_ip.pcap'))
     header, data = src.next()
     bpf = pcapy.compile(src.datalink(), 1350, "dst 4.3.2.1", 0, 1)
     assert matches_filter(data, bpf) is True
