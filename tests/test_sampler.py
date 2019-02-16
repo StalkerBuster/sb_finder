07437585db663ea446e3fefec4f5f8f3e39d47b9
ulif <uli@gnufix.de>
2019-02-16 14:08:21 +0100

    Add tests for upcoming sampler.

---
+++ tests/test_sampler.py
@@ -0,0 +1,21 @@
+from scapy.all import IP
+from sampler import SampleMaker
+
+
+class TestSampleMaker(object):
+    # tests for `SampleMaker`
+
+    def test_create(self):
+        # we can create SampleMaker instances
+        sm = SampleMaker()
+        assert sm is not None
+        assert sm.dev is None
+        assert sm.outpath == "sample.pcapng"
+
+    def test_update_outpath(self, tmpdir):
+        # we can append packets to an outfile
+        sample_path = tmpdir.join('test-update.pcap')
+        sm = SampleMaker(outpath=str(sample_path))
+        pkts = (IP(), IP())
+        sm.update_outpath(pkts)
+        assert sample_path.isfile()
