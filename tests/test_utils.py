43d9430a0dd3c49aa65efa360e363ca3774453bf
ulif <uli@gnufix.de>
2019-03-07 23:21:04 +0100

    Ensure, fake_ifconfig fixture works.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 from utils import get_avail_netdevs, get_avail_wlans
 
 
@@ -5,3 +7,10 @@ def test_get_avail_netdevs():
     devs = get_avail_netdevs()
     assert "lo" in devs
     assert type(devs) == list
+
+
+def test_fake_ifconfig(fake_ifconfig):
+    # ensure, the `fake_ifconfig` fixture works.
+    lines = subprocess.check_output("ifconfig").decode("utf-8")
+    assert os.path.exists(str(fake_ifconfig))
+    assert "foobar0:" in lines
