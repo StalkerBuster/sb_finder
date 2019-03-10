44fd9dcc5aa3fd3ffd3b96fe7d4f66017e6b81c7
ulif <uli@gnufix.de>
2019-03-10 15:59:56 +0100

    Add first real netdev test
    based on new `fake_ifconfig` fixture.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,9 +4,18 @@ from utils import get_avail_netdevs, get_avail_wlans
 
 
 def test_get_avail_netdevs():
+    # yes, we can get any network device name
     devs = get_avail_netdevs()
     assert "lo" in devs
     assert type(devs) == list
+
+
+def test_get_avail_netdevs_can_extract_names(fake_ifconfig):
+    # we can get an expected set of device names
+    devs = get_avail_netdevs()
+    assert "foobar0" in devs
+    assert "lo" in devs
+    assert "wlp1s0" in devs
 
 
 def test_fake_ifconfig(fake_ifconfig):
